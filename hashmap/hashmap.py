36368492dbea048a0d2d339efb9399b8e9632a0f
danipopp <popp.daniel16@icloud.com>
2025-12-19 23:12:49 +0100

    put and robin hood condtion

--- hashmap/hashmap.py
+++ hashmap/hashmap.py
@@ -6,4 +6,35 @@ class RobinHoodHashMap:
     def _hash(self, key):
         return hash(key) % self.capacity
     
-    
+    def put(self, key, value):
+        index = self._hash(key)
+        probe_distance = 0
+
+        while True:
+            entry = self.table[index]
+
+            # index is empty
+            if entry is None:
+                self.table[index] = (key, value, probe_distance)
+                return
+            
+            exist_key, exist_value, exist_distance = entry
+
+            # update existing key
+            if exist_key == key:
+                self.table[index] = (key, value, exist_distance)
+                return
+            
+            # robin hood condition
+            if exist_distance < probe_distance:
+                # steal slot
+                self.table[index] = (key, value, probe_distance)
+
+                # find new spot
+                key = exist_key
+                value = exist_value
+                probe_distance = exist_distance
+            
+            # Move index to next slot
+            index = (index + 1) % self.capacity
+            probe_distance += 1
