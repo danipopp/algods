42798c89387e39d63ff92d5238199e8888106f2b
danipopp <popp.daniel16@icloud.com>
2025-12-19 22:39:52 +0100

    Initilize Test file

---
+++ hashmap/tests.py
@@ -0,0 +1,10 @@
+from hashmap import RobinHoodHashMap
+
+def test_put_and_get():
+    hm = RobinHoodHashMap()
+
+def run_all_tests():
+    test_put_and_get()
+
+if __name__ == "__main__":
+    run_all_tests()
