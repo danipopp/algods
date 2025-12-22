d4deefb6149e532fe97db5060d35cf8ff2b82a8e
danipopp <popp.daniel16@icloud.com>
2025-12-22 19:24:44 +0100

    left rotation

--- RedBlackTree/redblacktree.py
+++ RedBlackTree/redblacktree.py
@@ -16,4 +16,21 @@ class RedBlackTree:
         self.root = self.NIL
 
     # -------------- Rotations --------------
-    
+    def left_rotation(self, x):
+        y = x.right
+        x.right = y.left
+
+        if y.left != self.NIL:
+            y.left.parent = x
+
+        y.parent = x.parent
+
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+
+        y.left = x
+        x.parent = y
