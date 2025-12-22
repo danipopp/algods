de97d25884a340db1ae8431c259f3758952d3ed4
danipopp <popp.daniel16@icloud.com>
2025-12-22 19:19:51 +0100

    Init Red blac tree class

--- RedBlackTree/redblacktree.py
+++ RedBlackTree/redblacktree.py
@@ -4,7 +4,16 @@ BLACK = "BLACK"
 class Node:
     def __init__(self, key):
         self.key = key
-        self.coloar = RED
+        self.colar = RED
         self.parent = None
         self.left = None
         self.right = None
+
+class RedBlackTree:
+    def __init__(self):
+        self.NIL = Node(None)
+        self.NIL.colar = BLACK
+        self.root = self.NIL
+
+    # -------------- Rotations --------------
+    
